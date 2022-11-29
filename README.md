d78033a3433bae7369bb198c803261bd177c00c3
kidhrisalim <98416940+al-hajjar@users.noreply.github.com>
2022-11-29 18:30:19 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # code-challenge-phase1
+## Author:
+
+[Kidhri salim](https:yueyyt)
+
+## Description
+
+This project is a web application that calculate user grades after inputing their marks and detects speed and also calculates user net salary
+
+## Live Page
+
+
+
+## installation
+
+1. Open terminal(Ctrl+Alt+T)
+
+2. git clone ```git@github.com:al-hajjar/code-challenge-phase1.git```
+
+3. cd code-Challenge
+
+4. code . or atom . depending on the text editor of your choice.
+
+## Technologies Used
+
+1. [HTML](https://)
+2. [CSS](https://)
+3. [Javascript](https://)
+
+## BDD
+
+|Behaviour                | Input                                 |                                     output|
+| ----------------------- | ------------------------------------- | ----------------------------------------- |
+| Enter your grade        |  80                                   |   A                                       |
+| Enter yor speed         |  40/80/180                            |   OK/ 2 demerits points/license suspended |
+| Enter your basic salary |  80,000ksh                            |   64,000ksh                               |
+
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- main.js
+++ main.js
@@ -48,3 +48,98 @@ enter.addEventListener("click" , function (e){
     }
 
 });
+// Net Salary Calculator (Toy Problem)
+
+const calculation = document.getElementById("calculation");
+
+function formResetingg(){
+    document.getElementById("reseting").reset();
+}
+
+calculation.addEventListener("click", function (yow){
+    yow.preventDefault();
+    var basicSalary = document.getElementById("money").value;
+
+    console.log(basicSalary);
+    const percantage = function (){
+        if(basicSalary <= 24000){
+            return 10;
+        }else if(basicSalary > 24000 && basicSalary <= 32333){
+            return 25;
+        }else if(basicSalary > 32333){
+            return 30;
+        }
+    }
+    console.log(percantage);
+
+    const nhif = function (){
+        if (basicSalary <= 5900){
+            return 150;
+        }else if(basicSalary >= 6000 && basicSalary <= 7999){
+            return 300;
+        }else if(basicSalary > 7999 && basicSalary <= 11999){
+            return 400;
+        }else if(basicSalary > 11999 && basicSalary <= 14999){
+            return 500;
+        }else if(basicSalary > 14999 && basicSalary <= 19999){
+            return 600;
+        }else if(basicSalary > 19999 && basicSalary <= 24999){
+            return 750;
+        }else if(basicSalary > 24999 && basicSalary <= 29999){
+            return 850;
+        }else if(basicSalary > 29999 && basicSalary <= 34999){
+            return 900;
+        }else if(basicSalary > 34999 && basicSalary <= 39999){
+            return 950;
+        }else if(basicSalary > 39999 && basicSalary <= 44999){
+            return 1000;
+        }else if(basicSalary > 44999 && basicSalary <= 49999){
+            return 1100;
+        }else if(basicSalary > 49999 && basicSalary <= 59999){
+            return 1200;
+        }else if(basicSalary > 59999 && basicSalary <= 69999){
+            return 1300;
+        }else if(basicSalary > 69999 && basicSalary <= 79999){
+            return 1400;
+        }else if(basicSalary > 79999 && basicSalary <= 89999){
+            return 1500;
+        }else if(basicSalary > 89999 && basicSalary <= 99999){
+            return 1600;
+        }else if(basicSalary > 99999){
+            return 1700;
+        }
+    }
+    console.log(nhif);
+
+    const kra = (basicSalary * 10)/100;
+    const relief = 2400;
+    const tax = (basicSalary * percantage())/100;
+    const nssf = (basicSalary * 6)/100;
+    const netSalary = (basicSalary - tax - nssf - nhif() - kra) + relief;
+    const net = document.getElementById("net");
+    const azgar = document.getElementById("azgar");
+    const deductions = document.getElementById("deductions");
+    const khatwib = document.getElementById("khatwib");
+    const mohd = document.getElementById("mohd");
+    const faruk = document.getElementById("faruk");
+    const ahmed = document.getElementById("ahmed");
+    const abuu = document.getElementById("abuu");
+    const daudi = document.getElementById("daudi");
+    
+    console.log(netSalary);
+    net.innerHTML = "Net Salary: " + netSalary;
+    azgar.innerHTML = "Basic Salary: " + basicSalary;
+    deductions.innerHTML = "Deductions";
+    khatwib.innerHTML = "Tax: " + tax;
+    mohd.innerHTML = "NSSF: " + nssf;
+    faruk.innerHTML = "NHIF: " + nhif();
+    ahmed.innerHTML = "KRA: " + kra;
+    abuu.innerHTML = "RELIEF";
+    daudi.innerHTML = "RELIEF: " + relief;
+
+    // if (basicSalary <= 24000){
+    //     alert(basicSalary - ((basicSalary * 10)/100))
+    // }
+
+    formResetingg();
+});
